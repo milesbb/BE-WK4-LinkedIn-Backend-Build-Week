5c5cc343ed535d8bb72191e6a6edae41536bd330
mohammadnazamikit <108289537+mohammadnazamikit@users.noreply.github.com>
2022-10-24 16:26:50 +0200

    done with post

--- src/api/posts/postModel.js
+++ src/api/posts/postModel.js
@@ -5,13 +5,13 @@ import mongoose from "mongoose";
 const { Schema, model} =mongoose
 
 
-const userSchema = new Schema({
+/* const userSchema = new Schema({
     name: {type: String,required:true},
     surname: {type: String, required: true},
     image: {type: String, required: true}
 },{
     timestamps: true
-})
+}) */
 
 
 const postSchema = new Schema(
@@ -19,9 +19,9 @@ const postSchema = new Schema(
         text: {type: String, required: true},
         username: {type: String, required: true},
         image: {type: String, required: true},
-        user: [
+       /*  user: [
             userSchema
-        ]
+        ] */
     },{
         timestamps: true
     }
