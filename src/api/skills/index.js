d96c5f3d2f455bbdc93f4c7129e2d5c7db09c8b2
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-10-27 12:44:18 +0100

    added skills endpoints

--- src/api/skills/index.js
+++ src/api/skills/index.js
@@ -21,56 +21,36 @@ skillsRouter.post("/", async (req, res, next) => {
   }
 });
 
+// GET ALL POSSIBLE SKILLS
+
+skillsRouter.get("/", async (req, res, next) => {
+  try {
+    const skills = await SkillsModel.find();
+
+    if (skills) {
+      res.status(200).send(skills);
+    } else {
+      next(createHttpError(404, "No skills found"));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 // ADD SKILL(S) TO USER
 
-skillsRouter.put(
-    "/:userId/experiences/:experienceId",
-    async (req, res, next) => {
-      try {
-        const user = await UsersModel.findById(req.params.userId);
-  
-        if (user) {
-          const selectedExperienceIndex = user.experiences.findIndex(
-            (experience) => experience._id.toString() === req.params.experienceId
-          );
-  
-          if (selectedExperienceIndex !== -1) {
-            user.experiences[selectedExperienceIndex] = {
-              ...user.experiences[selectedExperienceIndex],
-              ...req.body,
-              _id: req.params.experienceId,
-            };
-  
-            await user.save();
-  
-            res.status(200).send(user);
-          } else {
-            next(
-              createHttpError(
-                404,
-                `Experience with id ${req.params.experienceId} not found!`
-              )
-            );
-          }
-        } else {
-          next(
-            createHttpError(404, `User with id ${req.params.userId} not found!`)
-          );
-        }
-      } catch (error) {
-        next(error);
-      }
-    }
-  );
-
-// GET USER SKILL(S)
-
-skillsRouter.get("/:userId/skills", async (req, res, next) => {
+skillsRouter.put("/:userId", async (req, res, next) => {
   try {
-    const user = await UsersModel.findById(req.params.userId);
+    const updatedUser = await UsersModel.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $push: { skills: { $each: req.body.skills } },
+      },
+      { new: true, runValidators: true }
+    );
 
-    if (user) {
-      res.status(200).send(user.experiences);
+    if (updatedUser) {
+      res.status(200).send(updatedUser.skills);
     } else {
       next(
         createHttpError(404, `User with id ${req.params.userId} not found!`)
@@ -81,31 +61,47 @@ skillsRouter.get("/:userId/skills", async (req, res, next) => {
   }
 });
 
+// GET USER SKILL(S)
 
+skillsRouter.get("/:userId", async (req, res, next) => {
+  try {
+    const user = await UsersModel.findById(req.params.userId).populate({
+      path: "skills",
+      select: "skillName _id",
+    });
+
+    if (user) {
+      res.status(200).send(user.skills);
+    } else {
+      next(
+        createHttpError(404, `User with id ${req.params.userId} not found!`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 // REMOVE SKILL
 
-skillsRouter.delete(
-  "/:userId/experiences/:experienceId",
-  async (req, res, next) => {
-    try {
-      const user = await UsersModel.findByIdAndUpdate(
-        req.params.userId,
-        { $pull: { experiences: { _id: req.params.experienceId } } },
-        { new: true }
-      );
+skillsRouter.delete("/:userId/remove/:skillId", async (req, res, next) => {
+  try {
+    const user = await UsersModel.findByIdAndUpdate(
+      req.params.userId,
+      { $pull: { skills: req.params.skillId } },
+      { new: true }
+    );
 
-      if (user) {
-        res.send();
-      } else {
-        next(
-          createHttpError(404, `User with id ${req.params.userId} not found!`)
-        );
-      }
-    } catch (error) {
-      next(error);
+    if (user) {
+      res.send();
+    } else {
+      next(
+        createHttpError(404, `User with id ${req.params.userId} not found!`)
+      );
     }
+  } catch (error) {
+    next(error);
   }
-);
+});
 
 export default skillsRouter;
